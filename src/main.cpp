e5c59d01d21cf8ac8a81785dc3f8aae9f54c28ba
jerrydie <shcheglovapolina@gmail.com>
2022-05-24 17:18:13 -0400

    Alpha loop

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv) {
 	size_t pt_bit_length = 8;
 	int shift = 64 - pt_bit_length; // сдвиг добавила для совместимости с их функциями
 	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> distribution(1, 15); // для маски 0|alpha
+	//std::uniform_int_distribution<> distribution(1, 15); // для маски 0|alpha
 	
 	// структуры для задания конфигурации
 	fpe_transform_ctx transform_ctx[1];
@@ -35,53 +35,64 @@ int main(int argc, char** argv) {
 	
 	fpe_tweak tweak;
 	// первая половина настройки фиксирована произвольной константой
-	tweak.tweak[0] = randomU64(gen);
+	//tweak.tweak[0] = randomU64(gen);
 	
-	//"максимальный" открытый текст, для цикла
-	u16 maximum_pt = std::pow(2, pt_bit_length);
 	
-	int alpha = distribution(gen); //генерируем alpha
-	u8 mask1 = alpha; //первая маска, вида 0|alpha
-	//std::cout << "Mask1: " << std::setw(9) << std::bitset<8>(mask1) << std::endl;
-	u8 mask2 = alpha << 4; // вторая маска? вида alpha|0
-	//std::cout << "Mask2: " << std::setw(9) << std::bitset<8>(mask2) << std::endl;
-	
-	double c1 = 0;
-	double c2 = 0;
-	for (u8 pt = 0; pt != maximum_pt - 1; pt += 1) // Перебираем все открытые тексты
+	//int alpha = distribution(gen); //генерируем alpha
+	for (int alpha = 1; alpha <=15; alpha++)
 	{
+		u8 mask1 = alpha; //первая маска, вида 0|alpha
+		//std::cout << "Mask1: " << std::setw(9) << std::bitset<8>(mask1) << std::endl;
+		u8 mask2 = alpha << 4; // вторая маска? вида alpha|0
+		//std::cout << "Mask2: " << std::setw(9) << std::bitset<8>(mask2) << std::endl;
+		
+		double c1 = 0;
+		double c2 = 0;
+		
+		//std::uniform_int_distribution<> fixed_distribution(1, 15);
+		//u8 fixed_pt = fixed_distribution(gen);
+		
+		//"максимальный" открытый текст, для цикла
+		u16 maximum_pt = std::pow(2, pt_bit_length) - 1;
+		//Количество материала
+		u16 qtty_pt = std::pow(2,8);
+		for (u8 pt = 0; pt <= maximum_pt; pt += 1) // Перебираем все открытые тексты
+		{
 
-		//std::cout << "Plaintext:          " << std::setw(9) << std::bitset<8>(pt) << std::endl;
-		//std::cout << "Plaintext & mask1:  " << std::setw(9) << std::bitset<8>(pt & mask1) << std::endl;
-		u64 XX[2] = {pt, 0};
-		//Вторая половина настройки, выбирается случайно
-		tweak.tweak[1] = randomU64(gen);
-		
-		// Шифруем с помощью встроенной функции
-		FEA_A_Enc_U64ToU64(encrypt_ctx->e_ctx.rk, tweak.tweak, XX, encrypt_ctx->t_ctx.block_bits, encrypt_ctx->e_ctx.rounds);
-		
-		//Получаем шифртекст, приводим к нужному виду
-		u8 ct = XX[1] >> shift;
-		//std::cout << "Ciphertext:         " << std::setw(9) << std::bitset<8>(ct) << std::endl;
-		//std::cout << "Ciphertext & mask1: " << std::setw(9) << std::bitset<8>(ct & mask1) << std::endl << std::endl;
-		if ((ct & mask1) == (pt & mask1)) // проверяем выполнение соотношения для первой маски
-		{
-			c1+= 1;
-			//std::cout << "C1: " << c1 <<std::endl;
-			//std::cout << std::endl << std::endl << std::endl << "YEEEEEEEEES#1" << std::endl << std::endl << std::endl;
+			//std::cout << "Plaintext:          " << std::setw(9) << std::bitset<8>(pt) << std::endl;
+			//std::cout << "Plaintext & mask1:  " << std::setw(9) << std::bitset<8>(pt & mask1) << std::endl;
+			u64 XX[2] = {pt, 0};
+			//Вторая половина настройки, выбирается случайно
+			tweak.tweak[0] = randomU64(gen);
+			tweak.tweak[1] = randomU64(gen);
+			
+			// Шифруем с помощью встроенной функции
+			FEA_A_Enc_U64ToU64(encrypt_ctx->e_ctx.rk, tweak.tweak, XX, encrypt_ctx->t_ctx.block_bits, encrypt_ctx->e_ctx.rounds);
+			
+			//Получаем шифртекст, приводим к нужному виду
+			u8 ct = XX[1] >> shift;
+			//std::cout << "Ciphertext:         " << std::setw(9) << std::bitset<8>(ct) << std::endl;
+			//std::cout << "Ciphertext & mask1: " << std::setw(9) << std::bitset<8>(ct & mask1) << std::endl << std::endl;
+			if ((ct & mask1) == (pt & mask1)) // проверяем выполнение соотношения для первой маски
+			{
+				c1+= 1;
+				//std::cout << "C1: " << c1 <<std::endl;
+				//std::cout << std::endl << std::endl << std::endl << "YEEEEEEEEES#1" << std::endl << std::endl << std::endl;
+			}
+			if ((ct & mask2) == (pt & mask2)) // проверяем выполнение соотношения для второй маски
+			{
+				c2+= 1;
+				//std::cout << "C2: " << c2 <<std::endl;
+				//std::cout << std::endl << std::endl << std::endl << "YEEEEEEEEES#1" << std::endl << std::endl << std::endl;
+			}
+			if (pt == maximum_pt) break;
 		}
-		if ((ct & mask2) == (pt & mask2)) // проверяем выполнение соотношения для второй маски
-		{
-			c2+= 1;
-			//std::cout << "C2: " << c2 <<std::endl;
-			//std::cout << std::endl << std::endl << std::endl << "YEEEEEEEEES#1" << std::endl << std::endl << std::endl;
-		}
+		c1 = std::pow((2*c1/qtty_pt - 1), 2);
+		c2 = std::pow((2*c2/qtty_pt - 1), 2);
+		
+		// Вычисляем практические значения корреляции и выводим на экран для сравнения
+		std::cout << "0|alpha experimental correlation: " << c1 << std::endl;
+		std::cout << "alpha|0 experimental correlation: " << c2 << std::endl << std::endl;
 	}
-	c1 = abs(2*c1/maximum_pt - 1);
-	c2 = abs(2*c2/maximum_pt - 1);
-	
-	// Вычисляем практические значения корреляции и выводим на экран для сравнения
-	std::cout << "0|alpha experimental correlation: " << c1 << std::endl;
-	std::cout << "alpha|0 experimental correlation: " << c2 << std::endl;
 }
 
